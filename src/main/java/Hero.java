ca34f886786ea354592675feffc3704b0be867ae
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-28 08:29:02 +0300

    add function to Hero.java  to get hero name

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,5 +1,14 @@
 public class Hero{
-  public Hero(String name){
+  //member property to hold hero name
+  private String mName;
 
+  //Hero class with a constructor that (for now) takes only one argument: the superhero's name
+  public Hero(String name){
+    mName = name;
+  }
+
+  //function to return a hero's name
+  public String getName(){
+    return mName;
   }
 }
