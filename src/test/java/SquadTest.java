57e70bfed01ee870fc9e8aa9bec3e09f44287098
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-28 15:46:20 +0300

    add test for instantiation with name

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -5,7 +5,13 @@ public class SquadTest {
 
   @Test
   public void squad_instantiatesCorrectly_true() {
-    Squad testSquad = new Squad("Home");
+    Squad testSquad = new Squad("Justice League");
     assertEquals(true, testSquad instanceof Squad);
+  }
+
+  @Test
+  public void getName_squadInstantiatesWithName_Home() {
+    Squad testSquad = new Squad("Justice League");
+    assertEquals("Justice League", testSquad.getName());
   }
 }
