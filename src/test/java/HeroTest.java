26a17311c4c701696503ab19e10b5248f487b878
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-28 10:25:14 +0300

    dry out testing code, create index.vtl and add root route in App.java file that will render home page

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,15 +2,21 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest{
+  Hero myHero;
+  @Before
+  public void instantiating() {
+    myHero = new Hero("Superman");
+  }
+
   @Test
   public void Hero_instantiatesCorrectly_true(){
-    Hero myHero = new Hero("Superman");
+    // Hero myHero = new Hero("Superman");
     assertEquals(true, myHero instanceof Hero);
   }
 
   @Test
   public void Hero_instantiatesWithName_String() {
-    Hero myHero = new Hero("Superman");
+    // Hero myHero = new Hero("Superman");
     assertEquals("Superman", myHero.getName());
   }
 }
