2135fe3c97e82fe6d289c4262cf611cdd67ef4b6
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-28 09:25:01 +0300

    add private hero properties

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,6 +1,9 @@
 public class Hero{
   //member property to hold hero name
   private String mName;
+  private int mAge;
+  private String mSpecialPower;
+  private String mWeakness;
 
   //Hero class with a constructor that (for now) takes only one argument: the superhero's name
   public Hero(String name){
