6af5b08ec63391911617b7285e425e3119c825ba
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-28 10:59:29 +0300

    resolve errors in App.java, create post method for new heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
-import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -13,6 +15,7 @@ public class App {
 
   get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("hero", request.session().attribute("hero"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -20,16 +23,22 @@ public class App {
     post("/heroes", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
+      ArrayList<Hero> heroes = request.session().attribute("heroes");
+      if (heroes == null) {
+        heroes = new ArrayList<Hero>();
+        request.session().attribute("heroes", heroes);
+      }
+
       String name = request.queryParams("name");
       int age = Integer.parseInt(request.queryParams("age"));
       String specialPower = request.queryParams("specialPower");
       String weakness = request.queryParams("weakness");
       Hero newHero = new Hero(name, age, specialPower, weakness);
-      request.session().attribute("hero", newHero);
+      heroes.add(newHero);
 
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+     }, new VelocityTemplateEngine());
 
   }
 }
