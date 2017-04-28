0daa9f985e7a32e1ff73b026c18c22cedc3ee74d
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-28 11:04:48 +0300

    modify App.java and index.vtl to display multiple heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,7 +15,7 @@ public class App {
 
   get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("hero", request.session().attribute("hero"));
+      model.put("heroes", request.session().attribute("heroes"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
