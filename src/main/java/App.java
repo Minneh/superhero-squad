bb92b83be8e0435659bfa3605184bc80069670f0
Minneh Mugo <minneh.mugo@gmail.com>
2017-04-28 16:44:33 +0300

    add route and form for new heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -38,6 +38,12 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/squads/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/squad-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     post("/heroes", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
