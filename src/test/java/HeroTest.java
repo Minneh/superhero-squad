7852360bba48670cd554389201f3f84b4cd19efd
Minneh Mugo <minneh.mugo@gmail.com>
2017-05-07 11:29:16 +0300

    remove instantiating function from HeroTest

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,11 +2,6 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest{
-  Hero myHero;
-  @Before
-  public void instantiating() {
-    myHero = new Hero("Superman", 35, "Flight", "Kryptonite");
-  }
 
   @Test
   public void Hero_instantiatesCorrectly_true(){
